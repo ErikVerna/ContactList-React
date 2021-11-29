c3dfb3dd15d6f986a30655163c0be97fadbd4f0e
ErikVerna <vernaerik@gmail.com>
2021-11-29 15:50:50 -0300

    edited name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <nav className="navbar navbar-dark bg-black">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">Agenda React + Reducer</span>
+        <span className="navbar-brand mb-0 h1">
+          Contact List React + Reducer
+        </span>
       </div>
     </nav>
   );
